be3ea177e9273f1aa34feb43564004db39405cd9
poobool-koon <mygm1302@gmail.com>
2021-05-09 13:16:46 +0900

    UI works

--- app/src/main/java/com/example/swordcard/MainActivity.java
+++ app/src/main/java/com/example/swordcard/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +19,33 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("HELLO");
         a.removeWord("Car");
         System.out.println("Words:"+a.getAllWords());
-
-        Button btn = (Button)findViewById(R.id.OpenSub);
+        
+        //촬영모드
+        btn = (Button)findViewById(R.id.OpenCam);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,SubActivity.class);
+                Intent intent = new Intent(MainActivity.this,CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //단어장모드
+        Button btn = (Button)findViewById(R.id.OpenList);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,WordListActivity.class);
+                startActivity(intent);
+            }
+        });
+        
+        //퀴즈모드
+        btn = (Button)findViewById(R.id.OpenQuiz);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,QuizActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/swordcard/QuizActivity.java
+++ app/src/main/java/com/example/swordcard/QuizActivity.java
@@ -1,20 +1,18 @@
 package com.example.swordcard;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SubActivity extends AppCompatActivity {
+public class QuizActivity extends AppCompatActivity {
     public Button btn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sub);
+        setContentView(R.layout.activity_wordlist);
 
         btn = (Button)findViewById(R.id.Close);
         btn.setOnClickListener(new View.OnClickListener(){
